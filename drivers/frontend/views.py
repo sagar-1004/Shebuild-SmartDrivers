96ed15433810991d532d52742c97109169d0b231
Sagar <sagarrahangdale1004@gmail.com>
2023-01-07 12:35:31 +0530

    funct, added

--- drivers/frontend/views.py
+++ drivers/frontend/views.py
@@ -1,22 +1,59 @@
-from rest_framework import status, viewsets
-import requests
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.decorators import action
-from rest_framework import serializers
-from django.core.files.storage import FileSystemStorage
-from django.core.files.base import ContentFile
 from django.shortcuts import render, HttpResponse, redirect
-import csv
-from .models import *
-from django.http import HttpResponse
-from django.shortcuts import render
-# from .serializer import*
-# Create your views here.
-
-# Generate CSV File lab_elmts List
+from dateutil.relativedelta import relativedelta
+import datetime
+from backend.models import *
+import requests
+from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers import json
+from django.http import JsonResponse
+from django.core import serializers
+import json
+from django.contrib.auth import get_user_model
+from uuid import UUID
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers import serialize
+import re
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout  # add this
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm  # add this
+from django.contrib.auth.models import User
 
 
 def index(request):
     return render(request, 'index.html')
 
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				id = user.id
+
+				return redirect('/viewskill')
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request, 'login.html', context={"login_form": form})
+
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful.")
+			return redirect('/')
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render(request, 'signup.html', context={"register_form": form})
